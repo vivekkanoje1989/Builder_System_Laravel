52bb7b48fae424e38f0a956cd0597699dee6e0b7
NE-MANOJ <manoj@nextedgegroup.co.in>
2017-08-17 18:25:05 +0530

    17082017

--- app/Modules/Reports/Views/teamFollowupReport_old.blade.php
+++ app/Modules/Reports/Views/teamFollowupReport_old.blade.php
@@ -1,14 +1,11 @@
-
 <div class="row">
     <div class="widget flat radius-bordered">
-        <div class="col-lg-12 col-sm-12 col-xs-12" ng-controller="newenquiryController" ng-init="teamFollowupReport([[$loggedInUserID]])">
+        <div class="col-lg-12 col-sm-12 col-xs-12" ng-controller="reportsController" ng-init="teamFollowupReport([[$loggedInUserID]])">
+            <h5 class="row-title before-themeprimary"><i class="fa  fa-arrow-circle-o-right themeprimary"></i>{{headingName}}</h5>
             <div class="widget-body bordered-top bordered-themeprimary col-lg-12 col-sm-12 col-xs-12">
                 <div id="status-report">
-                    <div class="widget">                                
-                        <div class="widget-header">
-                            <span class="widget-caption" style="font-size: 15px;font-weight: 600 !important;">Team Followup's Report</span>
-                        </div>
-                        <div class="widget-body table-responsive">
+                    <div class="widget"> 
+                        <div class="table-responsive">
                             <table class="table table-hover table-striped table-bordered" at-config="config">
                                 <thead class="bord-bot">
                                     <tr>
@@ -22,7 +19,7 @@
                                 </thead>
                                 <tbody>
                                     <tr ng-repeat="followup in team_followup_report">
-                                        <td ng-if="followup.is_parent == 1 && followup.employee_id != employee_id"><a href="" ng-if="followup.is_parent == 1 && followup.employee_id != employee_id" ng-click="getteamfollowupReport(followup, $index,<?php echo Auth::guard('admin')->user()->id; ?>)">{{followup.name}}<i class="icon" style="float: right" ng-class="isSubEmployeeShown ? 'ion-chevron-up' : 'ion-chevron-down'"></i></a></div>
+                                        <td ng-if="followup.is_parent == 1 && followup.employee_id != employee_id"><a href="" ng-if="followup.is_parent == 1 && followup.employee_id != employee_id" ng-click="getteamfollowupReport(followup)">{{followup.name}}<i class="icon" style="float: right" ng-class="isSubEmployeeShown ? 'ion-chevron-up' : 'ion-chevron-down'"></i></a></div>
                                         <td ng-if="followup.is_parent == 0 || followup.employee_id == employee_id">{{followup.name}}</div>
                                         <td><b>{{followup.total}}</b></td>
                                         <td>{{followup.sameday}}</td>
@@ -58,6 +55,63 @@
                             <div class="col-md-12 col-xs-12"  align="center" >
                                 <div style="margin:0 auto;padding: 40px; width: 30%">
                                     <canvas id="doughnut" class="chart chart-doughnut" chart-data="followupdata" chart-options="followupoptions" chart-labels="followuplabels" chart-colors="followupcolors"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                
+                
+                
+                
+                         <div class="widget-body table-responsive" ng-if="subteam_followup_report.length > 0">
+                            <table class="table table-hover table-striped table-bordered" at-config="config">
+                                <thead class="bord-bot">
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Total</th>
+                                        <th>Same Day</th>
+                                        <th>Second Day</th>
+                                        <th>Third Day</th>
+                                        <th>After Third Day</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr ng-repeat="followup in subteam_followup_report">
+                                        <td ng-if="followup.is_parent == 1 && followup.employee_id != employee_id"><a href="" ng-if="followup.is_parent == 1 && followup.employee_id != employee_id" ng-click="getteamfollowupReport(followup)">{{followup.name}}<i class="icon" style="float: right" ng-class="isSubEmployeeShown ? 'ion-chevron-up' : 'ion-chevron-down'"></i></a></div>
+                                        <td ng-if="followup.is_parent == 0 || followup.employee_id == employee_id">{{followup.name}}</div>
+                                        <td><b>{{followup.total}}</b></td>
+                                        <td>{{followup.sameday}}</td>
+                                        <td>{{followup.secondday}}</td>
+                                        <td>{{followup.thirdday}}</td>
+                                        <td>{{followup.afterthirdday}}</td>
+                                    </tr>   
+                                    <tr>
+                                        <td><b>Total</b></td>
+                                        <td><b>{{subtotal}}</b></td>
+                                        <td><b>{{subtotalSame}}</b></td>
+                                        <td><b>{{subtotalSecond}}</b></td>
+                                        <td><b>{{subtotalThird}}</b></td>
+                                        <td><b>{{subtotalAfter}}</b></td>
+                                    </tr>  
+                                    <tr>
+                                        <td><b>Total (%)</b></td>
+                                        <td ng-if="subtotal > '0'"><b>{{((subtotal / subtotal) * 100).toFixed(2)}}</b></td>
+                                        <td ng-if="subtotal == '0'"><b>0.00</b></td>
+                                        <td ng-if="subtotalSame > '0'"><b>{{((subtotalSame / subtotal) * 100).toFixed(2)}}</b></td>
+                                        <td ng-if="subtotalSame == '0'"><b>0.00</b></td>
+                                        <td ng-if="subtotalSecond > '0'"><b>{{((subtotalSecond / subtotal) * 100).toFixed(2)}}</b></td>
+                                        <td ng-if="subtotalSecond == '0'"><b>0.00</b></td>
+                                        <td ng-if="subtotalThird > '0'"><b>{{((subtotalThird / subtotal) * 100).toFixed(2)}}</b></td>
+                                        <td ng-if="subtotalThird == '0'"><b>0.00</b></td>
+                                        <td ng-if="subtotalAfter > '0'"><b>{{((subtotalAfter / subtotal) * 100).toFixed(2)}}</b></td>
+                                        <td ng-if="subtotalAfter == '0'"><b>0.00</b></td>
+                                    </tr>  
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="row"  align="center" >
+                            <div class="col-md-12 col-xs-12"  align="center" >
+                                <div style="margin:0 auto;padding: 40px; width: 30%">
+                                    <canvas id="doughnut" class="chart chart-doughnut" chart-data="subfollowupdata" chart-options="subfollowupoptions" chart-labels="subfollowuplabels" chart-colors="subfollowupcolors"></canvas>
                                 </div>
                             </div>
                         </div>
