c9741d0ce871bf65a0f731917df06f5c46026535
NE-MANOJ <manoj@nextedgegroup.co.in>
2017-11-03 08:52:31 +0530

    03112017

--- app/Modules/Api/routes/api.php
+++ app/Modules/Api/routes/api.php
@@ -3,5 +3,4 @@
 Route::group(array('module' => 'Api', 'middleware' => ['api'], 'namespace' => 'App\Modules\Api\Controllers'), function() {
 
        Route::get('api/pushapi/BmsPushApi', 'ApiController@actionIndex');
-    
 });	
