04eb688b9ad0c2a8cdbf82b3373099da9e7e744c
archana <archana@nextedgegroup.co.in>
2017-11-07 14:55:15 +0530

    updated

--- app/Modules/MasterHr/Views/profile.blade.php
+++ app/Modules/MasterHr/Views/profile.blade.php
@@ -1,6 +1,7 @@
 <div class="row" ng-controller="hrController" ng-init="getProfile()">
     <div class="col-lg-6 col-sm-6 col-xs-12">
-        <form ng-submit="frmProfile.$valid && updateProfile(profileData)"  name="frmProfile"  novalidate enctype="multipart/form-data"  >
+  
+        <form ng-submit="frmProfile.$valid && invalidImage == '' && updateProfile(profileData)"  name="frmProfile"  novalidate enctype="multipart/form-data"  >
             <input type="hidden" ng-model="csrfToken" name="csrftoken" id="csrftoken" ng-init="csrfToken = '<?php echo csrf_token(); ?>'" class="form-control">
             <div class="widget flat radius-bordered">
                 <div class="widget-header bordered-bottom bordered-themeprimary">
@@ -38,30 +39,53 @@
                             </div>
                         </div>
 
-                        <div class="row">
+<!--                        <div class="row">
                             <div class="col-sm-12">
                                 <div class="form-group">
                                     <label id="lblpp" for="">Profile picture<span class="sp-err">*</span></label>
-                                      <span class="input-icon icon-right">
-                                            <input type="file" ngf-select name="employee_photo_file_name" id="employee_photo_file_name"   ng-model="profileData.employee_photo_file_name" ng-change="checkImageExtension(profileData.employee_photo_file_name);" id="employee_photo_file_name" ngf-max-size="3MB" accept="image/x-png,image/gif,image/jpeg,image/bmp,image/jpg"    class="form-control imageFile"  ngf-model-invalid="errorFile" required> 
+                                    <span class="input-icon icon-right">
+                                        <input type="file" ngf-select name="employee_photo_file_name" id="employee_photo_file_name"   ng-model="profileData.employee_photo_file_name" ng-change="checkImageExtension(profileData.employee_photo_file_name);" id="employee_photo_file_name" ngf-max-size="3MB" accept="image/x-png,image/gif,image/jpeg,image/bmp,image/jpg"    class="form-control imageFile"  ngf-model-invalid="errorFile" required> 
                                         <div ng-show="btnProfile || invalidImage"   ng-messages="frmProfile.employee_photo_file_name.$error" class="help-block">
-                                                <div id="err_pp" ng-message="required" class="sp-err">Please select profile picture must be less than 3 MB</div>
-                                                <div ng-if="invalidImage" class="sp-err">{{invalidImage}}</div>
-                                       </div>
-                                            
+                                            <div id="err_pp" ng-message="required" class="sp-err">Please select profile picture.</div>
+                                            <div ng-if="invalidImage" class="sp-err">{{invalidImage}}</div>
+                                        </div>
                                     </span> 
                                 </div>
                             </div>
-                        </div> 
+                        </div> -->
+    <!--ngf-max-size=3MB  3MB=3145728  -->
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <label id="lblpp" for="">Profile Picture <span class="sp-err">*</span> </label>
+                                        <span class="input-icon icon-right">
+                                        <input type="file" ngf-select name="employee_photo_file_name" id="employee_photo_file_name" ng-model-options="{ allowInvalid: true, debounce: 300 }"  ng-model="profileData.employee_photo_file_name" ng-change="checkImageExtension(profileData.employee_photo_file_name);checkImagesize(profileData.employee_photo_file_name)" id="employee_photo_file_name"  accept="image/x-png,image/gif,image/jpeg,image/bmp,image/jpg"    class="form-control imageFile"   ngf-model-invalid="errorFile" required> 
+                                        <div ng-show="btnProfile || invalidImage || invalidImagesize"   ng-messages="frmProfile.employee_photo_file_name.$error" class="help-block">
+                                                <div id="err_pp" ng-message="required" class="sp-err">Please Select Profile Picture </div>
+                                                <div ng-if="invalidImage" class="sp-err">{{invalidImage}}</div>
+                                                <div ng-if="invalidImagesize" class="sp-err">{{invalidImagesize}}</div>
+                                       </div>
+                                                                           
+                                        <!--Profile picture cannot be blank -->
+                                        <div class="img-div2" data-title="name" ng-repeat="list in employee_photo_file_name_preview">    
+                                               <img ng-src="{{list}}" class="thumb photoPreview">
+                                           </div>
+                                        <div ng-show="(!employee_photo_file_name_preview) && (flag_profile_photo == 1)">
+                                            <img ng-src="{{old_profile_photo}}" class="thumb photoPreview"/>
+                                        </div>
+                                    </span>  
+                                    </div>
+                                </div>
+                            </div> 
 
                         <div class="row">
                             <div class="col-lg-3 cl-xs-3">
-                                <div ng-repeat="list in employee_photo_file_name_preview">    
+<!--                                <div ng-repeat="list in employee_photo_file_name_preview">    
                                     <img ng-src="{{list}}" class="thumb" style="width: 60%;"/>
                                 </div>
                                 <div ng-show="(!employee_photo_file_name_preview) && (flag_profile_photo == 1)">
                                     <img ng-src="{{old_profile_photo}}" class="thumb" style="width: 60%;"/>
-                                </div>
+                                </div>-->
                             </div>
                             <div class="col-lg-9 cl-xs-9" align="center">
                                 <button type="submit" class="btn btn-primary" id="btn_update_profile" ng-click="btnProfile = true" style="margin-top: 7%;" ng-disabled="profileBtn">Update Profile</button>
@@ -98,7 +122,7 @@
                                         <input type="password" check-old-password ng-model="profileData.oldPassword" id="oldPassword"  name="oldPassword" maxlength="15" class="form-control"  maxlength="15"  required ng-model-options="{ allowInvalid: true, debounce: 300 }">
                                         <div ng-show="btnfrmPassword"   ng-messages="frmPassword.oldPassword.$error" class="help-block">
                                             <div id="err_old_required" ng-message="required" class="sp-err">This field is required.</div>
-                                             <div id="err_new_pass_maxlength" ng-message="maxlength" class="sp-err">Maximum 15 Characters Allowed.</div>
+                                            <div id="err_new_pass_maxlength" ng-message="maxlength" class="sp-err">Maximum 15 Characters Allowed.</div>
                                             <div id="err_old_pass_match" ng-message="compareOldPassword" class="sp-err">Password could not be matched</div>
                                         </div>
                                         <div ng-if="oldPassword" class="sp-err oldPassword">{{oldPassword}}</div>
@@ -114,7 +138,7 @@
                                         <input type="password" ng-model="profileData.password"  ng-pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/" name="password"  maxlength="15"  ng-minlength="6"  class="form-control" required>
                                         <div ng-show="btnfrmPassword" ng-messages="frmPassword.password.$error" class="help-block">
                                             <div id="err_new_pass_blank" ng-message="required" class="sp-err" >This field is required.</div>
-                                            <div id="err_new_pass_minlength" ng-message="minlength" class="sp-err">Minimum 6 Characters Allowed.</div>
+                                            <div id="err_new_pass_minlength" ng-message="minlength" class="sp-err">Minimum 6 and Maximum 15 Characters Allowed.</div>
                                             <div id="err_new_pass_maxlength" ng-message="maxlength" class="sp-err">Maximum 15 Characters Allowed.</div>
                                             <div id="err_new_pass_pattern" ng-message="pattern" class="sp-err" >Password must contain at least one uppercase letter, one lowercase letter, one number and one special character</div>
 
