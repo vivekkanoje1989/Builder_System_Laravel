1af2afd5b7e62494422d6a7bd9aaa2ff239145cd
sudhirwani8793@gmail.com <sudhirwani8793@gmail.com>
2017-11-18 11:07:08 +0530

    issues

--- public/backend/customerController.js
+++ public/backend/customerController.js
@@ -31,10 +31,11 @@ app.controller('customerController', ['$scope', '$state', 'Data', 'Upload', '$ti
         $scope.showaddress = true;
         $scope.hideaddress = false;
         $scope.customerAddress = false;
-
         $scope.todayremarkTimeChange = function (selectedDate)
         {
-            //$scope.enquiryData.next_followup_time = '';
+            if($scope.enquiryData.id <= 0){
+            $scope.enquiryData.next_followup_time = '';    
+            }            
             if (typeof selectedDate == 'undefined') {
                 $scope.timeList = [];
                 $scope.enquiryData.next_followup_time = '';
@@ -56,7 +57,6 @@ app.controller('customerController', ['$scope', '$state', 'Data', 'Upload', '$ti
                 });
             }
         }
-
         $scope.getModulesWiseHist_list = function (enquiry_id, opt, flag)
         {
             if (opt == 1)
@@ -500,7 +500,7 @@ var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
             $scope.enquiryData.parking_required = $scope.enquiryData.finance_required = "0";
             date.setHours("10");
             date.setMinutes("00");
-            $scope.enquiryData.next_followup_time = date;
+            //$scope.enquiryData.next_followup_time = date;
             if (customerId !== 0 && enquiryId === 0) {
                 $scope.showloader();
                 Data.post('master-sales/getCustomerDataWithId', {
